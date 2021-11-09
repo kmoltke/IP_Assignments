49858adc85c37528d256365db6943848c1a91b0a
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2021-11-09 23:51:11 +0100

    comment out message

--- Assignment_10/NotCheckedInException.java
+++ Assignment_10/NotCheckedInException.java
@@ -1,5 +1,5 @@
 public class NotCheckedInException extends Exception{
-    private String message;
+    // private String message;
     public NotCheckedInException(){
         super("Error: Cannot check out; Not currently checked in");
         // System.out.println("Error: Cannot check out; Not currently checked in");
