9cd3ee3e78a259d3cd0b1ee6abbc21f0c0d9e922
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2021-11-09 19:40:29 +0100

    declare fields and methods

---
+++ Assignment_10/RejseKort.java
@@ -0,0 +1,34 @@
+/**
+ * @author Kristian Moltke Reitzel
+ * 
+ * RejseKort
+ * 
+ */
+public class RejseKort {
+    private int balance;
+    private boolean isCheckedIn;
+
+
+
+    public RejseKort() {
+        balance = 100;
+        isCheckedIn = false;
+    }
+
+    public void depositMoney(int dkk) {
+
+    }
+
+    public boolean isCheckedIn(int timeStamp) {
+        return true;
+    }
+
+    public void checkIn(int x, int y, int timeStamp) {
+
+    }
+
+    public void checkOut(int x, int y, int timeStamp) {
+
+    }
+    
+}
