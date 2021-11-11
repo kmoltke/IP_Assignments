23826f2f3caaa533092d2173dadb1a51f49b6dde
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2021-11-11 11:33:19 +0100

    Add .java files

---
+++ Assignment_7/Order.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    protected List<Food> ordered;
+
+    public Order() {
+        this.ordered = new ArrayList<>();
+    }
+
+    public void addFood(Food food) {
+        ordered.add(food);
+    }
+
+    public int total() {
+        int total = 0;
+        for (Food food : ordered) {
+            total += food.getPrice();
+        }
+
+        return total;
+    }
+
+    public void display() {
+
+        for (Food food : ordered) {
+            food.display();
+            System.out.println();
+        }
+
+        System.out.println(total() + " kr TOTAL");
+    }
+
+    public boolean payWith(CreditCard card) {
+        if (card.withdraw(total()) == false) {
+            System.out.println("ERROR: Payment failed");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    /*
+    public static void main(String[] args) {
+        Pizza p1 = new Pizza();
+        Food b1 = new Food("Burger", 65);
+        Food s1 = new Food("Salad", 30);
+        p1.addTopping("Pepperoni");
+        p1.addTopping("Onions");
+        Order order1 = new Order();
+        order1.addFood(p1);
+        order1.addFood(b1);
+        order1.addFood(s1);
+
+        // p1.display();
+        order1.display();
+    }
+    */
+}
---
+++ Assignment_7/Pizza.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza extends Food {
+    protected List<String> toppings;   // List of toppings
+
+    public Pizza(){
+        super("Pizza", 45);
+        this.toppings = new ArrayList<>();
+    }
+
+    public void addTopping(String topping) {
+        toppings.add(topping);
+        price += 10;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        String toppingStr = " { ";
+        toppingStr += String.join(", ", toppings);
+        toppingStr += " }";
+        
+        return toppingStr;
+    }
+
+    @Override
+    public void display(){
+        super.display();
+        System.out.print(toString());
+    }
+}
